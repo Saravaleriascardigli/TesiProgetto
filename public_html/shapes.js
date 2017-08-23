cf8a36ee1f694d20ee6d1b49150010e73c8c07de
Yunasummoner <saravaleriascardigli@yahoo.it>
2017-08-23 18:40:39 +0200

    Correzioni, selezione arco

--- public_html/shapes.js
+++ public_html/shapes.js
@@ -19,75 +19,59 @@ var	dx = x - point.x,
 	dy = y - point.y;
 return dx*dx + dy*dy; }
 
-//	calcola e restituisce 1 o 2 in base al fatto che le coordinate x e y
-//	siano più vicine rispettivamente al primo o al secondo punto di item
+//	calcola e restituisce il quadrato della distanza 
+//	dall'estremo più lontano alle coordinate x e y
+function maxDist2(item, x, y) {
+return Math.max(dist2(item.a, x, y), dist2(item.b, x, y)) }
+
+//	calcola e restituisce l'estremo più vicino alle coordinate x e y
 function nearest(item, x, y) {
-return (dist2(item.a, x, y) < dist2(item.b, x, y)) ? 1 : 2 }
+return (dist2(item.a, x, y) < dist2(item.b, x, y)) ? item.a : item.b }
+
+//	calcola e restituisce il centro del quarto di circonferenza
+function arcCenter(item) {
+var	c = [],
+	sumX = item.b.x + item.a.x,
+	difX = item.b.x - item.a.x,
+	sumY = item.b.y + item.a.y,
+	difY = item.b.y - item.a.y,
+	verso = (item.shape == "rev") ? +1 : -1;
+	c.x = (sumX + verso * difY) / 2;
+	c.y = (sumY - verso * difX) / 2;
+return c; }
 
 //	restituisce la descrizione testuale della linea per il path dello SVG
 function toLine(item) {
 return ["M", item.a.x, item.a.y, "L", item.b.x, item.b.y].join(" "); }
 
-//	restituisce la descrizione testuale dell'arco per il path dello SVG: 
-//	i punti a e b di item rappresentano gli estremi di un quarto d'arco 
-//	di ellisse, da tracciare in senso orario o anti-orario, 
-//	a seconda del valore di verso
-function toEllArc(item) {
-var	rx = item.b.x - item.a.x,
-	ry = item.b.y - item.a.y,
-	verso = (item.shape == "ellRev") ? 0 : 1;
-return ["M", item.a.x, item.a.y, "A", rx, ry, 0, 0, verso, item.b.x, item.b.y].join(" "); }
-
-
-//	restituisce la descrizione testuale dell'arco per il path dello SVG
-//	il punto a di item rappresenta il centro dell'arco da tracciare
-//	il punto b di item rappresenta un punto sul quarto d'arco di circonferenza
-//	l'arco è sempre di 90 gradi e con gli assi paralleli ai cartesiani
-function toArc(item) {
-//	il raggio è pari alla distanza tra i due punti
-var radius = Math.sqrt(delta2(item)),
-//	dx e dy sono le differenze delle coordinate dei due punti
-	dx = item.b.x - item.a.x,
-	dy = item.b.y - item.a.y,
-//	calcolo adesso le coordinate dei due estremi dell'arco
-	ax = item.a.x,
-	ay = item.a.y + (dy > 0 ? radius : -radius),
-	bx = item.a.x + (dx > 0 ? radius : -radius),
-	by = item.a.y,
-//	se l'arco è nel secondo o nel quarto quadrante 
-//	allora inverto il verso di tracciamento
-	sweep = dx*dy < 0 ? 1 : 0;
-return ["M", ax, ay, "A", radius, radius, 0, 0, sweep, bx, by].join(" "); }
-
 //	restituisce la descrizione testuale dell'arco per il path dello SVG 
-//	ma, a differenza della funzione precedente, i punti a e b di item 
-//	già rappresentano gli estremi del quarto d'arco di circonferenza da 
-//	tracciare in senso orario, e inoltre non è applicabile il vincolo 
-//	del parallelismo degli assi dell'arco
-function toArc90(item) {
-//	in questo caso, l'unico calcolo necessario è quello del raggio,
-//	che è pari alla radice quadrata della metà della corda
-var radius = Math.sqrt(delta2(item)/2.0);
-const versoOrario = 1;
-return ["M", item.a.x, item.a.y, "A", radius, radius, 0, 0, versoOrario, item.b.x, item.b.y].join(" "); }
+//	i punti a e b di item rappresentano gli estremi del quarto d'arco di 
+//	circonferenza da tracciare in senso orario o anti-orario, 
+//	a seconda del tipo di shape
+function toArc(item) {
+return ["M", item.a.x, item.a.y, 
+	"A", item.radius, item.radius, 0, 0, item.verso, item.b.x, item.b.y].join(" "); }
 
 //	(ri)traccia le varie shape chiamando le opportune funzioni 
-//	per generare le corrispondenti descrizioni testuali
+//	per generare le corrispondenti descrizioni testuali SVG
 function redraw(item) {
 	switch (item.shape) {
 	case "point":
 	case "line":
 		return item.attr("d", toLine(item));
 	//	break;	// superfluo
-	case "ellArc": 
-	case "ellRev":
-		return item.attr('d', toEllArc(item));
-	//	break;	// superfluo
 	case "arc":
+	case "rev":
+	//	precalcolo il raggio e le coordinate del centro anche per altri usi (selezione)
+		item.center = arcCenter(item);
+		item.radius = Math.sqrt(delta2(item)/2.0);
+		//	essendo l'arco un quarto di circonferenza, il raggio è pari alla
+		//	lunghezza della corda diviso radice di due, quindi per semplicità 
+		//	divido direttamente per due delta2(active), che è il quadrato della 
+		//	lunghezza della corda, PRIMA di estrarne la radice quadrata
+		//	ossia sqrt(d*d) / strt(2) == sqrt(d*d/2)
+		item.verso = (item.shape == "rev") ? 0 : 1;
 		return item.attr('d', toArc(item));
-	//	break;	// superfluo
-	case "arc90":
-		return item.attr('d', toArc90(item));
 	//	break;	// superfluo
 	}
 }
@@ -99,6 +83,7 @@ function redraw(item) {
 	gestures - vettore che contiene gli SVG generati nel DOM
 	element - vettore che contiene i punti della shape usato solo per il log
 	shapeType - tipo della shape (linea, punto, arco)
+	singleStroke - 0/1, forza l'operatività in singlestroke
 	bgColor, shapeColor, activeColor - colori di background, shape e selezione attiva
 	modes - vettore che elenca le varie modalità operative
 	mode - modalità corrente
@@ -112,9 +97,6 @@ var bgColor = '#fff', shapeColor = '#557', activeColor = '#aaf',
 	modes = ["draw", "edit", "move"],
 	mode = "",
 
-//	*** PROVVISORIO *** testo descrittivo dello stato corrente del sistema 
-	defaultStatus = "Click on Palette to Start",
-
 //	imposto adesso il gestore degli eventi di trascinamento del mouse 
 //	(localizzato sullo SVG con ID univoco #main, che contiene la griglia 
 //	di fondo), generata direttamente nella dichiarazione dello stesso 
@@ -126,10 +108,22 @@ var bgColor = '#fff', shapeColor = '#557', activeColor = '#aaf',
 			.on("end", dragended)
 		);
 
-//	*** PROVVISORIO *** 
-//	imposta il valore iniziale della riga di stato 
+//	*** PROVVISORIO *** Gestione della RIGA DI STATO
 //	( testo descrittivo dello stato corrente del sistema )
-d3.select('#statusline').text(defaultStatus);
+function updateStatus() {
+const defaultStatus = "Click on Palette to Start";
+	//	se non è attiva nessuna modalità, mostro il testo predefinito
+	if (mode == "")
+		d3.select('#statusline').text(defaultStatus);
+	//	altrimenti stampo la modalità e il tipo di shape correnti
+	else if (mode == "draw")
+		d3.select('#statusline').text("draw " + shapeType);
+	else
+		d3.select('#statusline').text(mode + " " + active.shape);
+}
+
+//	imposta il valore iniziale della riga di stato 
+updateStatus();
 
 
 /*
@@ -137,7 +131,7 @@ d3.select('#statusline').text(defaultStatus);
 	e si occupa di verificare se il nuovo elemento generato debba essere
 	eliminato in quanto nullo, oppure accettato e in tal caso anche 
 	conservato nell'apposito stack delle shapes (inoltre le informazioni 
-	della shape sono inserite nel log del broser per comodità di debug)
+	della shape sono inserite nel log del browser per comodità di debug)
  */
 function dragended() { switch (mode) {
 case "edit":
@@ -162,7 +156,7 @@ var	element = [];	// contiene le informazioni sulla shape per il log
 			}
 			return ;
 		}
-		// salvo le coordinate di entrambi i punti della shape
+		//	salvo le coordinate di entrambi i punti della shape
 		element.xa = active.a.x;
 		element.ya = active.a.y;
 		element.xb = active.b.x;
@@ -173,6 +167,7 @@ var	element = [];	// contiene le informazioni sulla shape per il log
 	gestures.unshift(active);
 //	ed effettuo il log nella console
 	console.log(element);
+
 	break;
 }}
 
@@ -184,31 +179,38 @@ var	element = [];	// contiene le informazioni sulla shape per il log
 	caso restituisce 1 altrimenti 0
  */
 function checkSelection(x,y) { if (gestures.length > 1) {
-	var item = [], selected = -1, len, distA, distB;
+	var item = [], selected = -1;
 	for (var i = 0; i < gestures.length; i++) {
 		item = gestures[i];	// shape da testare
-		len =  Math.sqrt(delta2(item));	// distanza tra i due punti della shape
-	//	distanza di x e y rispettivamente dal primo e dal secondo punto della shape
-		distA = Math.sqrt(dist2(item.a, x, y));
-		distB = Math.sqrt(dist2(item.b, x, y));
 	//	procedo adesso a testare la distanza del click dalla shape
 	//	con tolleranza calcolata per tentativi ed appropriata ad ogni caso
 		switch (item.shape) {
 		case "point":
-			if (Math.abs(distA) < 3)
+		//	distanza di x e y dalla shape 
+		//	(si può testare indifferentemente uno dei due estremi, che coincidono)
+			if (Math.abs(Math.sqrt(dist2(item.a, x, y)) < 3))
 				selected = i;
 			break;
 		case "line":
 		//	se il click è sulla linea o nelle immediate vicinanze, 
 		//	allora la somma delle distanze dai due estremi sarà di 
 		//	poco superiore alla distanza tra gli estremi stessi
-			if (Math.abs(distA + distB - len) < 0.05)
+			if (Math.abs(
+					Math.sqrt(dist2(item.a, x, y)) 
+					+ Math.sqrt(dist2(item.b, x, y)) 
+					- Math.sqrt(delta2(item)))
+				< 0.05)
 				selected = i;
 			break;
 		case "arc":
+		case "rev":
 		//	se il click è sull'arco o nelle immediate vicinanze, allora 
-		//	la distanza dal centro sarà di poco superiore al raggio stesso
-			if (Math.abs(len - distA) < 2)
+		//	la distanza dal centro sarà di poco differente dal raggio stesso
+		//	ma inoltre, verifico anche che la massima distanza dai due estremi 
+		//	sia inferiore alla lunghezza della corda, per escludere che il punto 
+		//	si trovi all'esterno dell'arco, anche se pur sempre sulla circonferenza
+			if ((Math.abs(Math.sqrt(dist2(item.center, x, y)) - item.radius) < 3)
+				&& (maxDist2(item, x, y) <= delta2(item)))
 				selected = i;
 			break;
 		}
@@ -229,7 +231,9 @@ function checkSelection(x,y) { if (gestures.length > 1) {
 		gestures.splice(selected,1);
 	//	e lo reinserisco alla posizione 0
 		gestures.unshift(active);
-	//	infine restituisco 1 al chiamante
+	//	quindi aggiorno la riga di stato
+		updateStatus();
+	//	e infine restituisco 1 al chiamante
 		return 1;
 	}
 }	// in tutti gli altri casi restituisco 0
@@ -241,7 +245,7 @@ return 0; }
 	modalità operative, che sono attualmente tre: "draw", "move" e "edit"
 */
 
-function dragstarted() { var twinA, twinB, minA, minB, tmp;
+function dragstarted() { var point, twinA, twinB, minA, minB, tmp;
 switch (mode) {
 case "move":
 	//	se non è presente alcuna shape, oppure se è cambiata la shape selezionata
@@ -250,7 +254,7 @@ case "move":
 
 	if (singleStroke) 
 		d3.event.on("drag", function() {	// gestore dell'evento di trascinamento
-		//	in single stroke sposta tutte le shape della stessa entità
+		//	in singlestroke sposta tutte le shape della stessa entità
 		//	del movimento del mouse e quindi le ridisegna tutte
 			for (var i = 0; i < gestures.length; i++) {
 				gestures[i].a.x += d3.event.dx;
@@ -282,30 +286,23 @@ case "edit":
 		 || (active.shape == "point") )
 		return;
 
-	if (! singleStroke) { // multi stroke
-		//	se la shape è di tipo "arc"
-		//	oppure se l'evento è più vicino al secondo punto della shape
-		//	allora viene spostato solo il secondo punto
-		//	NOTA nel caso del tipo "arc", il primo punto è il centro della 
-		//		circonferenza, mentre il secondo è un punto sull'arco stesso
-		if ((active.shape == "arc") || (nearest(active, d3.event.x, d3.event.y) == 2))
-			d3.event.on("drag", function() {	// gestore dell'evento di trascinamento
-				active.b.x = d3.event.x;
-				active.b.y = d3.event.y;
-				redraw(active);
-			});
-		//	altrimenti, in tutti gli altri casi, viene spostato solo il primo punto
-		else
-			d3.event.on("drag", function() {	// gestore dell'evento di trascinamento
-				active.a.x = d3.event.x;
-				active.a.y = d3.event.y;
-				redraw(active);
-			});
-	} else { // single stroke
-		minA = 10000000000000000000;
-		minB = 10000000000000000000;
-		twinA = 0;
-		twinB = 0;
+	if (! singleStroke) { // multistroke
+		//	se sono in multistroke, devo spostare soltanto un estremo della 
+		// 	shape attiva, quello più vivino al click, che salvo in point
+		point = nearest(active, d3.event.x, d3.event.y);
+		d3.event.on("drag", function() {	// gestore dell'evento di trascinamento
+			//	aggiorno le coordinate del punto da trascinare
+			point.x = d3.event.x;
+			point.y = d3.event.y;
+			//	quindi ridisegno la shape
+			redraw(active);
+		});
+	} else { // singlestroke
+		//	altrimenti, se sono in singlestroke, devo individuare e salvare 
+		//	i due estremi "gemelli" delle shape consecutive da spostare insieme
+		minA = dist2(gestures[0].a, d3.event.x, d3.event.y);
+		minB = dist2(gestures[0].b, d3.event.x, d3.event.y);
+		twinA = twinB = gestures[0];
 		for (var i = 0; i < gestures.length; i++) {
 			tmp = dist2(gestures[i].a, d3.event.x, d3.event.y);
 			if (tmp < minA) { 
@@ -346,28 +343,28 @@ case "draw":
 	active.shape = shapeType;	//	il tipo di shape da tracciare
 	if (active.shape != "point") {
 		active.attr("marker-end", "url(#arrow)");
-		if (! singleStroke || (gestures.length == 0))
+		if (! singleStroke || (gestures.length == 0) || (gestures[0].shape == "point"))
 			active.attr("marker-start", "url(#dot)");
 	}
 	active.a = [];	//	il primo punto
 	active.b = [];	//	il secondo punto
 	if (singleStroke && (active.shape != "point") 
 		&& (gestures.length > 0) && (gestures[0].shape != "point")) {
-		// se in modalità single stroke e se non è un punto, 
+		// se in modalità singlestroke e se non è un punto, 
 		// il primo punto parte dal secondo dello stroke precedente, 
 		// se questo esiste e se non è un punto a sua volta
 		active.a.x = gestures[0].b.x;
-		active.b.x = d3.event.x;
 		active.a.y = gestures[0].b.y;
+		active.b.x = d3.event.x;
 		active.b.y = d3.event.y;
 	} else {
 		// in tutti gli altri casi, inizialmente i due punti sono uguali
 		active.a.x = active.b.x = d3.event.x;
 		active.a.y = active.b.y = d3.event.y;
 	}
-	//	quindi gli imposto il colore attivo...
+	//	quindi gli imposto il colore attivo e lo traccio per la prima volta
 	redraw(active).style("stroke", activeColor);
-	//	... e nel contempo imposto con colore non attivo 
+	//	e nel contempo imposto con colore non attivo 
 	//	l'elemento attivo in precedenza, se presente
 	if (gestures.length > 0)
 		gestures[0].style("stroke", shapeColor);
@@ -398,28 +395,23 @@ case "draw":
 
 
 /*
-	Il codice seguente rende attivi i pulsanti della palette.
+	Il codice seguente rende attivi e gestisce i pulsanti della palette.
 
 	In base all'indice del bottone, vengono definiti i parametri che saranno letti
 	dal gestore dell'evento per stabilire come interpretare il movimento del mouse 
 	e conseguentemente tradurlo nell'azione voluta.
+	
+	Il primo bottone attiva e disattiva l'operatività singlestroke.
 
-	I primi tre bottoni attivano la modalità "draw" e selezionano il tracciamento 
-	della shape corrispondente:	punto, linea e arco.
+	I successivi quattro bottoni attivano la modalità "draw" e selezionano il 
+	tracciamento della shape corrispondente: 
+		punto, linea, arco orario e arco anti-orario.
 
-	I successivi due, selezionano rispettivamente le modalità "edit" e "move".
+	I successivi due, selezionano rispettivamente le modalità "move" e "edit".
 
-	I due bottoni ancora successivi non alterano la modalità corrente, ma cambiano 
-	ciclicamente l'elemento correntemente attivo, rispettivamente "next" e "previous".
-
-	Il penultimo bottone svolge la funzione di eliminare l'elemento attivo e 
+	Infine, l'ultimo bottone svolge la funzione di eliminare l'elemento attivo e 
 	contemporaneamente portando attivo l'elemento sottostante quello eliminato.
 
-	Infine, l'ultimo bottone (aggiunto provvisoriamente) traccia una seconda
-	tipologia di arco, denominata "arc90", nella quale i due punti del trascinamento
-	rappresentano gli estremi di un arco pari ad un quarto di circonferenza,
-	tracciato in senso orario. Tuttavia, questo sistema non consente di imporre 
-	gli assi dell'arco paralleli ai cartesiani (a differenza del tipo "arc"). 
 
 
 	### Variabili Globali ###
@@ -428,29 +420,25 @@ case "draw":
 	action - voce del menu correntemente attiva
 */
 
-var	palette = ["single", "point", "line", "ellArc", "ellRev", "move", "edit", "del"],
-//	palette = ["point", "line", "arc", "arc90", "move", "edit", "del", "prev", "next"],
+var	palette = ["single", "point", "line", "arc", "rev", "move", "edit", "del"],
 	action = 0;
 
-//	attivo il gestore dell'evento click su tutti gli elementi "circle" di SVG
+//	attivo il gestore dell'evento click su tutti gli elementi ".button" di SVG
 d3.selectAll('.button').on('click', function(d, i) {
-//d3.selectAll('circle').on('click', function(d, i) {
-
-//	i = indice del cerchio cliccato
-//		se impostato a -1, non si deve cambiare l'action corrente
+//	i è l'indice del cerchio cliccato
+//	(nel codice lo imposto a -1 quando non si deve cambiare l'action corrente)
 var	newAction = palette[i];	//	stringa corrispondente al cerchio cliccato
 
 	switch (newAction) {
 	case "single": 
-		singleStroke = ! singleStroke;
+		singleStroke = ! singleStroke;  // inverto lo stato (on/off)
+		// imposto il colore in funzione dello stato attivo 
 		d3.select(this).style("fill", singleStroke ? "#def": "#fed");
 		break;
-	case "ellArc": 
-	case "ellRev":
 	case "point":
 	case "line":
 	case "arc":
-	case "arc90":
+	case "rev":
 	//	in caso di nuova shape da tracciare
 		mode = "draw";	//	imposto la modalità a "draw"
 		shapeType = newAction;	//	e salvo il tipo di shape richiesta
@@ -461,7 +449,7 @@ var	newAction = palette[i];	//	stringa corrispondente al cerchio cliccato
 		if (gestures.length > 0) //	se esiste almeno una shape
 			mode = newAction;	// allora imposto la nuova modalità
 		else	// altrimenti la ignoro e rimane la precedente
-			i = -1;	// e anche action non deve essere cambiata
+			i = -1;	// e anche l'action non deve essere cambiata
 		break;
 	case "del":
 		i = -1;	//	l'action corrente rimane inalterata
@@ -513,58 +501,18 @@ var	newAction = palette[i];	//	stringa corrispondente al cerchio cliccato
 		action.style("fill", "#def");	// e gli assegno il colore attivo
 	}
 
-//	*** PROVVISORIO *** Gestione della RIGA DI STATO
-//	( testo descrittivo dello stato corrente del sistema )
-
-	//	se non è attiva nessuna modalità, mostro il testo predefinito
-	if (mode == "")
-		d3.select('#statusline').text(defaultStatus);
-	//	altrimenti stampo la modalità e il tipo di shape correnti
-	else if (mode == "draw")
-		d3.select('#statusline').text("draw " + shapeType);
-	else
-		d3.select('#statusline').text(mode + " " + active.shape);
+	updateStatus();
 });
 
 /*
-d3.selectAll('circle')
-    .append('text')
-    .attr('class', 'glyphicon')
-    .text('\ue059');
+		//	precalcolo il raggio e le coordinate del centro per altri usi (selezione)
+		if ((active.shape == "arc") || (active.shape == "rev")) {
+			active.center = arcCenter(active);
+			active.radius = Math.sqrt(delta2(active)/2.0);
+		//	essendo l'arco un quarto di circonferenza, il raggio è pari alla
+		//	lunghezza della corda diviso radice di due, quindi per semplicità 
+		//	divido direttamente per due delta2(active), che è il quadrato della 
+		//	lunghezza della corda, PRIMA di estrarne la radice quadrata
 
-function toEllArc(item) {
-var	
-	rx = item.b.x - item.a.x,
-	ry = item.b.y - item.a.y,
-	cx = (item.b.x >= item.a.x) ^^ item.verso ? item.a.x : item.b.x,
-	cy = (item.b.y >= item.a.y) ^^ item.verso ? item.a.y : item.b.y;
-	if (dx*dx >= dy*dy) {
-		
-	} else {
-	}
-}
-
-return ["M", item.a.x, item.a.y, "A", Math.abs(rx), Math.abs(ry), 0, 0, 
-
-		tmp = dist2(gestures[i].a, d3.event.x, d3.event.y);
-		if (tmp < min) { 
-			k = i;
-			min = tmp; 
-			twinA = gestures[i];
-			if ((i > 0) && (gestures[i-1].shape != "point"))
-				twinB = gestures[i-1];
-			else
-				twinB = 0;
-		}
-		tmp = dist2(gestures[i].b, d3.event.x, d3.event.y);
-		if (tmp < min) { 
-			k = i; 
-			min = tmp; 
-			twinB = gestures[i];
-			if ((i+1 < gestures.length) && (gestures[i+1].shape != "point"))
-				twinA = gestures[i+1];
-			else
-				twinA = 0;
-		}
-
+//	d3.select('#statusline').text(defaultStatus);
 */
